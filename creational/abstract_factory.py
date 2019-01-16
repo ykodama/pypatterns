b38e3c47277abeb2d38821d03d091c6753b42219
Yu Kodama <kodama@mercari.com>
2019-01-16 12:17:55 +0900

    add some patterns

---
+++ creational/abstract_factory.py
@@ -0,0 +1,57 @@
+import random
+
+
+class PetShop(object):
+
+    """A pet shop"""
+
+    def __init__(self, animal_factory=None):
+        """pet_factory is our abstract factory.  We can set it at will."""
+
+        self.pet_factory = animal_factory
+
+    def show_pet(self):
+        """Creates and shows a pet using the abstract factory"""
+
+        pet = self.pet_factory()
+        print("We have a lovely {}".format(pet))
+        print("It says {}".format(pet.speak()))
+
+
+class Dog(object):
+    def speak(self):
+        return "woof"
+
+    def __str__(self):
+        return "Dog"
+
+
+class Cat(object):
+    def speak(self):
+        return "meow"
+
+    def __str__(self):
+        return "Cat"
+
+
+# Additional factories:
+
+# Create a random animal
+def random_animal():
+    """Let's be dynamic!"""
+    return random.choice([Dog, Cat])()
+
+
+# Show pets with various factories
+if __name__ == "__main__":
+
+    # A Shop that sells only cats
+    cat_shop = PetShop(Cat)
+    cat_shop.show_pet()
+    print("")
+
+    # A shop that sells random animals
+    shop = PetShop(random_animal)
+    for i in range(3):
+        shop.show_pet()
+        print("=" * 20)
---
+++ creational/factory_method.py
@@ -0,0 +1,68 @@
+class Building(object):
+    def __init__(self):
+        self.build_floor()
+        self.build_size()
+
+    def build_floor(self):
+        raise NotImplementedError
+
+    def build_size(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return 'Floor: {0.floor} | Size: {0.size}'.format(self)
+
+
+# Concrete Buildings
+class House(Building):
+    def build_floor(self):
+        self.floor = 'One'
+
+    def build_size(self):
+        self.size = 'Big'
+
+
+class Flat(Building):
+    def build_floor(self):
+        self.floor = 'More than One'
+
+    def build_size(self):
+        self.size = 'Small'
+
+
+# In some very complex cases, it might be desirable to pull out the building
+# logic into another function (or a method on another class), rather than being
+# in the base class '__init__'. (This leaves you in the strange situation where
+# a concrete class does not have a useful constructor)
+
+
+class ComplexBuilding(object):
+    def __repr__(self):
+        return 'Floor: {0.floor} | Size: {0.size}'.format(self)
+
+
+class ComplexHouse(ComplexBuilding):
+    def build_floor(self):
+        self.floor = 'One'
+
+    def build_size(self):
+        self.size = 'Big and fancy'
+
+
+def construct_building(cls):
+    building = cls()
+    building.build_floor()
+    building.build_size()
+    return building
+
+
+# Client
+if __name__ == "__main__":
+    house = House()
+    print(house)
+    flat = Flat()
+    print(flat)
+
+    # Using an external constructor function:
+    complex_house = construct_building(ComplexHouse)
+    print(complex_house)
